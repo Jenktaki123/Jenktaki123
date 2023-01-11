e557a92a781454c059ea1dd882127444582be054
Jenktaki123 <110096965+Jenktaki123@users.noreply.github.com>
2023-01-11 20:28:14 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import openai 
+
+def askGPT(text):
+  openai.api_key = ""
+  response = openai.Completion.create(
+    engine = "text-davinci-003",
+    prompt = text,
+    temperature = 8.6,
+    max_tokens = 150,
+     )
+     return print(response.choices[0].text)
+     
+     def main():
+       while True:
+         print('GPT: Ask me a question bobo ka\n')
+         python = input ()
+         askGPT(myOn)
+         print('\n')
+         
+         main()
